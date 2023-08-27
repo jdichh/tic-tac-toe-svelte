6f233975d76ecf5dbde4e9c2a92ca8c08df50896
jdichh <jpsdichoso21@gmail.com>
2023-08-27 12:11:57 +0800

    centered main container

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,27 @@
 <script>
   let buttons = new Array(9).fill(null);
   let result = null;
-  let turnIcon = 'X'
+  let turnIcon = "X";
 
   function setValue(i) {
     buttons[i] = turnIcon;
-    buttons = [...buttons]
-    turnIcon = turnIcon == "X" ? "O" : 'X'
+    buttons = [...buttons];
+    turnIcon = turnIcon == "X" ? "O" : "X";
   }
 </script>
 
 {#if !result}
-  <div class="container">
-    {#each buttons as button, i}
-      <button on:click={() => {setValue(i)}}>
-        {button ? button: ""}
-      </button>
-    {/each}
+  <div id="main">
+    <div class="container">
+      {#each buttons as button, i}
+        <button
+          on:click={() => {
+            setValue(i);
+          }}
+        >
+          {button ? button : ""}
+        </button>
+      {/each}
+    </div>
   </div>
-{:else}
-
-{/if}
+{:else}{/if}
