de1c069097dec623e7d865ac5e0a64a24fdb30d7
jdichh <jpsdichoso21@gmail.com>
2023-08-27 12:03:18 +0800

    X & O icons appearing inside buttons when pressed

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,23 @@
 <script>
   let buttons = new Array(9).fill(null);
   let result = null;
-  let turn = 'X'
+  let turnIcon = 'X'
+
+  function setValue(i) {
+    buttons[i] = turnIcon;
+    buttons = [...buttons]
+    turnIcon = turnIcon == "X" ? "O" : 'X'
+  }
 </script>
 
-<h1>Hello!</h1>
+{#if !result}
+  <div class="container">
+    {#each buttons as button, i}
+      <button on:click={() => {setValue(i)}}>
+        {button ? button: ""}
+      </button>
+    {/each}
+  </div>
+{:else}
+
+{/if}
